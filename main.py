9c408eec494d2c565805fd198c41a75e146c086d
sea-rod <seam22rodr3@gmail.com>
2025-04-12 18:22:41 +0530

    inital commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, Request, UploadFile, Form
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse("upload.html", {"request": request})
